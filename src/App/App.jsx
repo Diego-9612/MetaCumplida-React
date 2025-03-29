a650b0c5c5b3d5f06b2d0eb25060072024d3dfa6
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2025-03-28 19:03:17 -0500

    Correcciones

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,7 @@
 import "../App.css";
 import React from "react";
 import { AppUI } from "./AppUI";
-import { TareaProvider } from "../Componentes/TareaContext.jsx";
+import { TareaProvider } from "../Componentes/TareaContext";
 
 
 
